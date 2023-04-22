2e06275a5dd36aced54ffaaa037d86c14d2d8c04
Mostafa Saleh <mostafasaleh2055@gmail.com>
2023-04-22 12:51:51 +0200

    creating the basic structure of the app and linking files together

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { ChannelDetail, Feed, Navbar, SearchFeed, VideoDetail } from "./components/Exports";
+
+
+const App = () => {
+  return (
+    <div style={{fontSize: "30rem"}}>app</div>
+  );
+}
+
+export default App;
