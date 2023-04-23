3ac4992111a41a96762fa8e94c99c1632d57e713
Mostafa Saleh <mostafasaleh2055@gmail.com>
2023-04-23 16:04:54 +0200

    creating the sidebar and styling it, and making general rules for button

--- src/components/Exports.js
+++ src/components/Exports.js
@@ -4,3 +4,4 @@ export { default as Navbar } from "./Navbar";
 export { default as SearchFeed } from "./SearchFeed";
 export { default as VideoDetail } from "./VideoDetail";
 export { default as SearchBar } from "./SearchBar";
+export { default as SideBar } from "./SideBar";
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Stack } from "@mui/material";
+import { categories } from "../assets/MUIAssets";
+
+const selectedCategory = "New";
+
+const SideBar = () => {
+    return (
+        <Stack direction="row" sx={{
+            overflowY: "auto", height: { sx: "auto", md: "95%" }, flexDirection: { md: "column" },
+        }}
+        >
+            {categories.map((category) => (
+                <button
+                    className='category-btn'
+                    style={{ background: category.name === selectedCategory && "#fc1503", color: "white" }}
+                >
+                    <span style={{ color: category.name === selectedCategory ? "white" : "red", marginRight: "15px" }}>{category.icon}</span>
+                    <span style={{ opacity: category.name === selectedCategory ? 1 : 0.8 }}>{category.name}</span>
+                </button>
+            ))}
+        </Stack>
+    )
+}
+
+export default SideBar;
