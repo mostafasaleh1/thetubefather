78675f1b26d38ad1fffdec65d39c51314067ab15
Mostafa Saleh <mostafasaleh2055@gmail.com>
2023-04-23 11:59:58 +0200

    creating the structure of the feed section and changing the project name to the tubefather in package.json

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import { Box, Stack, Typography } from "@mui/material";
+import { BorderClear, BorderRight } from '@mui/icons-material';
+import {Link} from "react-router-dom";
+import SideBar from './SideBar';
 
 const Feed = () => {
   return (
-    <div>Feed</div>
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box sx={{ height: { sx: "auto", md: "92vh" }, BorderRight: "1px solid #3d3d3d", px: { sx: "0", md: 2 } }}>
+        <SideBar />
+        <Typography className='copyright' variant='body2' sx={{mt: 1.5, color: "#fff"}}>
+          Copyright &copy; 2023 <Link to="https://mostafasaleh1.github.io/myprofile" target='_blank' rel='noreferrer' color='#fff'>The Tubefather.</Link>
+        </Typography>
+      </Box>
+    </Stack>
   )
 }
 
