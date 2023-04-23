64929441f7842100b2266f2733d4a3751d73dede
Mostafa Saleh <mostafasaleh2055@gmail.com>
2023-04-23 03:56:47 +0200

    creating the navbar using material UI elements and its inline styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+import { Stack } from "@mui/material";
+import { logo } from "../assets/MUIAssets";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <Stack direction="row" alignItems="center" p={2} sx={{ position: "sticky", background: "#000", top: "0", justifyContent: "space-between" }}>
+      <Link to="/" style={{ display: "flex", alignItems: "center" }}>
+        <img src={logo} alt={logo} height={45} />
+      </Link>
+    </Stack>
   )
 }
 
