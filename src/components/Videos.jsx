644668b55e68e6cf918fb132e944b984e9fa5606
Mostafa Saleh <mostafasaleh2055@gmail.com>
2025-01-03 21:51:44 +0200

    finished Video Cards and fetched video data from API

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React from "react";
+import { Stack, Box } from "@mui/material";
+import { VideoCard, ChannelCard } from "./Exports";
 
-const Videos = ({ videos }) => {
-    console.log(videos);
-    return (
-        <div>Videos</div>
-    )
+const Videos = ({ videos, direction }) => {
+  
+  return (
+    <Stack direction={direction || "row"} flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
+      {videos.map((item, idx) => (
+        <Box key={idx}>
+          {item.id.videoId && <VideoCard video={item} /> }
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
+        </Box>
+      ))}
+    </Stack>
+  );
 }
 
 export default Videos;
